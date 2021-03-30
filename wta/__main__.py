f405442b9565b3c330bd158ee032fcf077f0a56b
mulasik <mulasik@gmail.com>
2021-03-30 22:56:01 +0200

    Split export and console output

--- wta/__main__.py
+++ wta/__main__.py
@@ -6,7 +6,8 @@ import os
 from .idfx_parser import IdfxParser
 from .sentence_history import SentenceHistoryGenerator
 from .visualisation import Visualisation
-from .export import (export_tpsfs_to_json, export_tpsfs_to_txt, export_sentence_history_to_json, output_revisions_number, export_sentence_history_to_txt)
+from .export import (export_tpsfs_to_json, export_tpsfs_to_txt, export_sentence_history_to_json, export_sentence_history_to_txt)
+from .console_output import output_revisions_number
 
 
 def load_path(dotted_path):
