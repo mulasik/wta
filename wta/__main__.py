6b3f6fc4d5caed3cbc0d505cb7c35edc0ac8538d
mulasik <mulasik@gmail.com>
2021-03-30 22:49:14 +0200

    Add text and sentence history visualisations

--- wta/__main__.py
+++ wta/__main__.py
@@ -5,8 +5,8 @@ import os
 
 from .idfx_parser import IdfxParser
 from .sentence_history import SentenceHistoryGenerator
-from .export import (export_tpsfs_to_json, export_tpsfs_to_txt, output_tpsfs_to_console, export_sentence_history_to_json,
-                     output_sentence_history_to_console, output_revisions_number, export_sentence_history_to_txt)
+from .visualisation import Visualisation
+from .export import (export_tpsfs_to_json, export_tpsfs_to_txt, export_sentence_history_to_json, output_revisions_number, export_sentence_history_to_txt)
 
 
 def load_path(dotted_path):
@@ -14,6 +14,7 @@ def load_path(dotted_path):
     module = import_module('.'.join(parts[:-1]))
     attr = getattr(module, parts[-1])
     return attr
+
 
 ECM = 'Edit Capturing Mode'
 PCM = 'Pause Capturing Mode'
@@ -28,38 +29,49 @@ if __name__ == "__main__":
     config = load_path(args.config)
 
     for idfx in config['xml']:
-        print(f'\n{idfx}\n')
+        print(f'\nProcessing the input file {idfx}...\n')
 
         # generate TPSFs
-        parser = IdfxParser(idfx, config['pause_duration'], config['edit_distance'], config['filtering'])
-        parser.run()
+        idfx_parser = IdfxParser(idfx, config['pause_duration'], config['edit_distance'], config['filtering'])
+        idfx_parser.run()
         file_name = os.path.split(idfx)[-1].replace('.idfx', '')
 
-        #ECM
-        export_tpsfs_to_json(parser.all_tpsfs_ecm, ECM, config['output'], file_name)
-        export_tpsfs_to_txt(parser.all_tpsfs_ecm, config['output'], file_name)
-        output_tpsfs_to_console(parser.all_tpsfs_ecm)
-        output_revisions_number(parser.all_tpsfs_ecm, ECM, False)
+        # ECM
+        export_tpsfs_to_json(idfx_parser.all_tpsfs_ecm, ECM, config['output'], file_name)
+        export_tpsfs_to_txt(idfx_parser.all_tpsfs_ecm, config['output'], file_name)
+        # output_tpsfs_to_console(idfx_parser.all_tpsfs_ecm)
+        # output_revisions_number(idfx_parser.all_tpsfs_ecm, ECM, False)
 
-        #PCM
-        export_tpsfs_to_json(parser.all_tpsfs_pcm, PCM, config['output'], file_name)
-        output_revisions_number(parser.all_tpsfs_pcm, PCM, False)
+        # PCM
+        export_tpsfs_to_json(idfx_parser.all_tpsfs_pcm, PCM, config['output'], file_name)
+        # output_revisions_number(idfx_parser.all_tpsfs_pcm, PCM, False)
 
-        #generate sentence history
-        sentence_history_generator = SentenceHistoryGenerator(parser.all_tpsfs_ecm)
+        # generate sentence history
+        sentence_history_generator = SentenceHistoryGenerator(idfx_parser.all_tpsfs_ecm)
         sentence_history = sentence_history_generator.sentence_history
-        filtered_sentence_history_wo_unchanged = sentence_history_generator.filter_sentence_history_wo_unchanged()
+        filtered_sentence_history = sentence_history_generator.filtered_sentence_history
+        sentences_selected_for_reparsing = sentence_history_generator.sentences_selected_for_reparsing
+
         export_sentence_history_to_json(sentence_history, config['output'], file_name)
-        export_sentence_history_to_json(filtered_sentence_history_wo_unchanged, config['output'], file_name + '_filtered')
+        export_sentence_history_to_json(filtered_sentence_history, config['output'], file_name, '_filtered')
+        export_sentence_history_to_json(sentences_selected_for_reparsing, config['output'], file_name, '_filtered_for_reparsing')
         export_sentence_history_to_txt(sentence_history, config['output'], file_name)
-        export_sentence_history_to_txt(filtered_sentence_history_wo_unchanged, config['output'], file_name, '_filtered')
-        output_sentence_history_to_console(filtered_sentence_history_wo_unchanged)
+        export_sentence_history_to_txt(filtered_sentence_history, config['output'], file_name, '_filtered')
+        export_sentence_history_to_txt(sentences_selected_for_reparsing, config['output'], file_name, '_filtered_for_reparsing')
+        # output_sentence_history_to_console(filtered_sentence_history)
+
+        # visualise
+        tpsfs_to_visualise = [tpsf for tpsf in idfx_parser.all_tpsfs_ecm if (len(tpsf.new_sentences) > 0 or len(tpsf.modified_sentences) > 0)]
+        visualisation = Visualisation(config['output'], file_name)
+        visualisation.visualise_text_history(tpsfs_to_visualise)
+        visualisation.visualise_sentence_history(tpsfs_to_visualise, sentence_history)
 
         if config['filtering'] is True:
-            relevant_tpsfs = [tpsf for tpsf in parser.all_tpsfs_ecm if tpsf.morphosyntactic_relevance is True]
-            export_tpsfs_to_json(relevant_tpsfs, ECM, config['output'], file_name, '_relevant')
-            export_tpsfs_to_txt(relevant_tpsfs, config['output'], file_name, '_relevant')
+            relevant_tpsfs = [tpsf for tpsf in idfx_parser.all_tpsfs_ecm if (len(tpsf.new_sentences) > 0 or len(tpsf.modified_sentences) > 0) and tpsf.morphosyntactic_relevance is True]
+            export_tpsfs_to_json(relevant_tpsfs, ECM, config['output'], file_name, '_filtered')
+            export_tpsfs_to_txt(relevant_tpsfs, config['output'], file_name, '_filtered')
             output_revisions_number(relevant_tpsfs, ECM, True)
-
-
+            visualisation_filtered = Visualisation(config['output'], file_name, '_filtered')
+            visualisation_filtered.visualise_text_history(relevant_tpsfs)
+            visualisation_filtered.visualise_sentence_history(relevant_tpsfs, sentence_history)
 
---
+++ wta/visualisation.py
@@ -0,0 +1,161 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as pltc
+import os
+
+
+class Visualisation:
+
+    def __init__(self, output_directory, file_name, filtered=''):
+        self.output_directory = output_directory
+        self.file_name = file_name
+        self.filtered = filtered
+
+    def visualise_text_history(self, tpsfs_to_visualise):
+
+        sentences_lengths = {}
+        for tpsf in tpsfs_to_visualise:
+            sentences_lens = []
+            transformation = 'insertion' if tpsf.transforming_sequence.label == 'append' else tpsf.transforming_sequence.label
+            for s in tpsf.sentence_list:
+                if s.text in [sen.text for sen in tpsf.unchanged_sentences]:
+                    label = 'unchanged'
+                elif s.text in [sen.text for sen in tpsf.new_sentences]:
+                    label = 'new'
+                elif s.text in [sen.text for sen in tpsf.modified_sentences]:
+                    label = f'modified through {transformation}'
+                elif s.text in [sen.text for sen in tpsf.deleted_sentences]:
+                    label = 'deleted'
+                sentences_lens.append((label, len(s.text)))
+            sentences_lengths.update({tpsf.revision_id: sentences_lens})
+
+        sentence_colors = {
+            'unchanged': 'mistyrose',
+            'new': 'darkslategrey',
+            'modified through insertion': 'cadetblue',
+            'modified through deletion': 'lightcoral',
+            'deleted': 'indianred'
+        }
+
+        transforming_colors = {
+            'deletion': 'indianred',
+            'insertion': 'darkslategrey',
+        }
+
+        tpsf_labels = []
+        for key in sentences_lengths.keys():
+            text = f'TPSF {key}'
+            tpsf_labels.append(text)
+
+        fig_height = len(tpsfs_to_visualise) * 0.80
+        fig_width = fig_height * 0.8
+        fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(fig_width, fig_height), gridspec_kw={'width_ratios': [4, 1]})
+
+        plt.yticks(ticks=list(sentences_lengths.keys()), labels=tpsf_labels)
+
+        ax1.set_ylim(len(sentences_lengths) + 1, -1)
+        ax1.tick_params(axis='both', which='major', labelsize=7)
+        ax1.set_yticklabels(tpsf_labels)
+
+        ax2.set_ylim(len(sentences_lengths) + 1, -1)
+        ax2.tick_params(axis='both', which='major', labelsize=7)
+        ax2.set_yticklabels(tpsf_labels)
+
+        ax1.label_outer()
+        ax2.label_outer()
+
+        for id, sens in sentences_lengths.items():
+            starts = 0
+            for s in sens:
+                lbl = f'{s[0]} sentences' if s[0] in ['new', 'deleted', 'unchanged'] else f'sentences {s[0]}'
+                ax1.barh(str(id), s[1], left=starts, height=1, color=sentence_colors[s[0]], edgecolor='white', label=lbl)
+                starts += s[1]
+
+        for tpsf in tpsfs_to_visualise:
+            lbl = 'insertion' if tpsf.transforming_sequence.label == 'append' else tpsf.transforming_sequence.label
+            transforming_seq_text = tpsf.transforming_sequence.text
+            ax2.barh(str(tpsf.revision_id), len(transforming_seq_text), left=0, height=1, label=lbl, color=transforming_colors[lbl], edgecolor='white')
+
+        ax1.set_xlabel('Number of characters')
+        ax2.set_xlabel('Number of characters')
+        ax1.set_title('Text History', pad=5)
+        ax2.set_title('Transforming sequence', pad=5)
+
+        # legend:
+        hand, labl = ax1.get_legend_handles_labels()
+        handout = []
+        lablout = []
+        for h, l in zip(hand, labl):
+            if l not in lablout:
+                lablout.append(l)
+                handout.append(h)
+        ax1.legend(handout, lablout, loc="upper right")
+        hand2, labl2 = ax2.get_legend_handles_labels()
+        handout2 = []
+        lablout2 = []
+        for h2, l2 in zip(hand2, labl2):
+            if l2 not in lablout2:
+                lablout2.append(l2)
+                handout2.append(h2)
+        ax2.legend(handout2, lablout2, loc="upper right")
+
+        plt.subplots_adjust(bottom=0.1, right=0.8, top=0.4, wspace=0.02)
+        fig_file = os.path.join(self.output_directory, f'{self.file_name}_text_history_visualisation{self.filtered}.svg')
+        plt.savefig(fig_file, bbox_inches='tight')
+
+    def visualise_sentence_history(self, tpsfs_to_visualise, sentence_history):
+
+        unrecommended_colors = ['white', 'snow', 'whitesmoke', 'seashell', 'antiquewhite', 'oldlace', 'floralwhite',
+                                'cornsilk',
+                                'ivory', 'honeydew', 'aliceblue', 'mintcream', 'azure', 'ghostwhite', 'lavenderblush',
+                                'beige', 'bisque', 'black']
+        available_colors = [k for k, v in pltc.cnames.items() if k not in unrecommended_colors]
+
+        sen_colors = {}
+        for id, sen_id in enumerate(list(sentence_history.keys())):
+            sen_colors.update({sen_id: available_colors[id]})
+
+        tpsf_sentences = {}
+        for tpsf in tpsfs_to_visualise:
+            tpsf_sens = []
+            for id, sen in enumerate(tpsf.sentence_list):
+                for sen_id, sen_list in sentence_history.items():
+                    if sen.text.strip() in [s.text for s in sen_list] and sen.text.strip() not in [s[2] for s in tpsf_sens]:
+                        sen_color = available_colors[int(id)]
+                        tpsf_sens.append((len(sen.text), sen_color, sen.text))
+                if sen.text.strip() not in [s[2] for s in tpsf_sens]:
+                    tpsf_sens.append((len(sen.text), 'beige', sen.text))
+            tpsf_sentences.update({tpsf.revision_id: tpsf_sens})
+
+        tpsf_labels = []
+        for key in tpsf_sentences.keys():
+            text = f'TPSF {key}'
+            tpsf_labels.append(text)
+
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(20, 20))
+        ax1.set_ylim(len(tpsf_sentences) + 1, -1)
+        ax1.tick_params(axis='both', which='major', labelsize=7)
+        ax1.set_title('Sentence History', pad=5)
+
+        for id, sens in tpsf_sentences.items():
+            a1_starts = 0
+            for s in sens:
+                ax1.barh(f'TPSF {id}', s[0], left=a1_starts, height=1, color=s[1], edgecolor='white', alpha=0.7)
+                a1_starts += s[0]
+
+        last_tpsf_id = list(tpsf_sentences.keys())[-1]
+        ax2.set_title(f'Last Sentence Versions (TPSF {last_tpsf_id}):', pad=5, horizontalalignment='left')
+        ax2.axis('off')
+        last_sen_versions = tpsf_sentences[last_tpsf_id]
+        a2_starts = 0.95
+        for s in last_sen_versions:
+            a2_starts -= 0.01
+            ax2.text(0, a2_starts, s[2].strip(), wrap=True, size=7, bbox={'facecolor': s[1], 'alpha': 0.6, 'pad': 2})
+            a2_starts -= 0.01
+
+        ax1.set_xlabel('Number of characters')
+
+        plt.subplots_adjust(bottom=0.1, right=0.8, top=0.4, wspace=0.01)
+        plt.tight_layout()
+        fig_file = os.path.join(self.output_directory, f'{self.file_name}_sentence_history_visualisation{self.filtered}.svg')
+        plt.savefig(fig_file, bbox_inches='tight')
+
