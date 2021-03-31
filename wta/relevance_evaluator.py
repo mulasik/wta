107d063244baeb300fd61c17cac3a7da74627c0a
mulasik <mulasik@gmail.com>
2021-03-31 22:21:54 +0200

    Extend relevancy evaluation with sentence evaluation

--- wta/relevance_evaluator.py
+++ wta/relevance_evaluator.py
@@ -11,35 +11,39 @@ class RelevanceEvaluator:
         self.morphosyntactic_relevance_eval_results = []
         self.set_morphosyntactic_relevance()
 
-    def evaluate_morphosyntactic_relevance(self):
-        for sen in self.tpsf.modified_sentences:
-            # affected_tokens is a list of tuples: previous word, current word with their indices
-            affected_tokens = retrieve_affected_tokens(sen)
-            # edited_tokens = filter_out_irrelevant_tokens(affected_tokens)  # TODO consider if it might be useful
-            if len(affected_tokens) > 0:
-                edit_distance, is_any_tok_oov, self.morphosyntactic_relevance = analyse_affected_tokens(affected_tokens, self.edit_distance_margin)
-            else:
-                # if no tokens are affected the TPSF is not relevant
-                self.morphosyntactic_relevance = False
-                is_any_tok_oov = None
-                edit_distance = None
-            self.morphosyntactic_relevance_eval_results.append({
-                sen.pos_in_text: {
-                    'number_affected_tokens': len(affected_tokens),
-                    'affected_tokens': affected_tokens,
-                    'is_any_tok_oov': is_any_tok_oov,
-                    'edit_distance': edit_distance,
-                }
-            })
+    def evaluate_sen_morphosyntactic_relevance(self, sen):
+        # affected_tokens is a list of tuples: previous word, current word with their indices
+        affected_tokens = retrieve_affected_tokens(sen)
+        # edited_tokens = filter_out_irrelevant_tokens(affected_tokens)  # TODO consider tokens filtering
+        if len(affected_tokens) > 0:
+            edit_distance, is_any_tok_oov, sentence_morphosyntactic_relevance = analyse_affected_tokens(affected_tokens,
+                                                                                                        self.edit_distance_margin)
+        else:
+            # if no tokens are affected the TPSF is not relevant
+            edit_distance, is_any_tok_oov, sentence_morphosyntactic_relevance = None, None, False
+        return affected_tokens, edit_distance, is_any_tok_oov, sentence_morphosyntactic_relevance
+
+    def capture_relevance_eval_results(self, sen, affected_tokens, is_any_tok_oov, edit_distance):
+        self.morphosyntactic_relevance_eval_results.append({
+            sen.pos_in_text: {
+                'number_affected_tokens': len(affected_tokens),
+                'affected_tokens': affected_tokens,
+                'is_any_tok_oov': is_any_tok_oov,
+                'edit_distance': edit_distance,
+                'sentence_morphosyntactic_relevance': sen.sentence_morphosyntactic_relevance
+            }
+        })
 
     def determine_morphosyntactic_relevance(self):
-        if len(self.tpsf.modified_sentences) > 0:
-            # if any sentence in the TPSF has been edited, evaluate the relevance of the edits
-            self.evaluate_morphosyntactic_relevance()
-        else:
-            # if no sentence modifications are detected, immediately mark the TPSF as irrelevant
-            self.morphosyntactic_relevance_eval_results = None
-            self.morphosyntactic_relevance = False
+        sentence_relevance_results = []
+        # if any sentence in the TPSF has been edited, evaluate the relevance of the edits
+        sens_to_evaluate = self.tpsf.modified_sentences + self.tpsf.new_sentences + self.tpsf.deleted_sentences
+        for sen in sens_to_evaluate:
+            affected_tokens, edit_distance, is_any_tok_oov, sen_morphosyntactic_relevance = self.evaluate_sen_morphosyntactic_relevance(sen)
+            sen.set_sentence_morphosyntactic_relevance(sen_morphosyntactic_relevance)
+            self.capture_relevance_eval_results(sen, affected_tokens, edit_distance, is_any_tok_oov)
+            sentence_relevance_results.append(sen_morphosyntactic_relevance)
+        self.morphosyntactic_relevance = True if True in sentence_relevance_results else False
 
     def set_morphosyntactic_relevance(self):
         if self.filtering is True:
