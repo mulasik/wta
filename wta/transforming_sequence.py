9419f56424b72d62688c20f22975a6874af538ce
mulasik <mulasik@gmail.com>
2021-03-31 22:23:33 +0200

    Minor adjustment

--- wta/transforming_sequence.py
+++ wta/transforming_sequence.py
@@ -1,4 +1,4 @@
-from .utils.nlp import tag_words, contains_end_punctuation_internally
+from .utils.nlp import tag_words, contains_end_punctuation_in_the_middle
 
 
 class TransformingSequence:
@@ -7,7 +7,7 @@ class TransformingSequence:
         self.text = text
         self.label = label
         self.tagged_tokens = None if text is None else tag_words(self.text)
-        self.contains_end_punctuation_internally = contains_end_punctuation_internally(self.text)
+        self.contains_end_punctuation_in_the_middle = contains_end_punctuation_in_the_middle(self.text)
 
     def __str__(self):
         printed_text = '' if self.text is None else f'{self.label}: {self.text}'
