0f060266ef715f9e0c6df1fb8d0399ac43fd2e7e
mulasik <mulasik@gmail.com>
2021-03-31 22:49:02 +0200

    Extend sentence filtering

--- wta/sentence_history.py
+++ wta/sentence_history.py
@@ -1,5 +1,6 @@
 import uuid
 from .sentence import Sentence
+from .utils.nlp import retrieve_match_range
 
 
 class SentenceHistoryGenerator:
@@ -8,6 +9,8 @@ class SentenceHistoryGenerator:
         self.tpsfs = tpsfs
         self.sentence_history = {}
         self.generate_sentence_history()
+        self.filtered_sentence_history = {}
+        self.filter_sentence_history()
 
     def generate_sentence_history(self):
         global_new_sens = []
@@ -25,15 +28,15 @@ class SentenceHistoryGenerator:
                         texts = [s.text for s in sens]
                         if ns.text in texts:
                             existing_id = id
-                            ns.set_revision_relevance(revision_relevance)
+                            ns.set_revision_morphosyntactic_relevance(revision_relevance)
                             self.sentence_history[existing_id].append(ns)
                 else:
                     uid = uuid.uuid1().int
-                    ns.set_revision_relevance(revision_relevance)
+                    ns.set_revision_morphosyntactic_relevance(revision_relevance)
                     self.sentence_history[uid] = [ns]
                 global_new_sens.append(ns)
             for es in modified_sens:
-                es.set_revision_relevance(revision_relevance)
+                es.set_revision_morphosyntactic_relevance(revision_relevance)
                 for id, sens in self.sentence_history.items():
                     sens_texts = [sen.text for sen in sens]
                     if es.previous_sentence.text in sens_texts:
@@ -48,48 +51,49 @@ class SentenceHistoryGenerator:
                             label = 'deleted'
                         sentence_remains = Sentence(None, None, None, ds.revision_id, None, None)
                         sentence_remains.set_label(label)
-                        sentence_remains.set_revision_relevance(revision_relevance)
+                        sentence_remains.set_revision_morphosyntactic_relevance(revision_relevance)
                         self.sentence_history[id].append(sentence_remains)
             for us in unchanged_sens:
-                us.set_revision_relevance(revision_relevance)
+                us.set_revision_morphosyntactic_relevance(revision_relevance)
                 for id, sens in self.sentence_history.items():
                     sens_texts = [sen.text for sen in sens]
                     if us.text in sens_texts:
                         self.sentence_history[id].append(us)
 
-    def filter_sentence_history_wo_unchanged(self):
-        filtered_sentence_history = {}
-        for id, sen_versions in self.sentence_history.items():
-            filtered_sentence_history[id] = []
-            for sv in sen_versions:
-                if sv.label != 'unchanged':
-                    if sv.label != 'modified':
-                        filtered_sentence_history[id].append(sv)
-                    elif sv.label == 'modified' and sv.revision_relevance is True:
-                        filtered_sentence_history[id].append(sv)
-                    else:
-                        continue
-            # add the last sentence version
-            last_sentence_version = sen_versions[-1]
-            if last_sentence_version.text not in [sv.text for sv in filtered_sentence_history[id]]:
-                filtered_sentence_history[id].append(last_sentence_version)
-        return filtered_sentence_history
-
     def filter_sentence_history(self):
-        filtered_sentence_history = {}
         for id, sen_versions in self.sentence_history.items():
-            filtered_sentence_history[id] = []
-            for sv in sen_versions:
-                if sv.label == 'new' or (sv.label != 'unchanged' and sv.revision_relevance is True):
-                    filtered_sentence_history[id].append(sv)
-            # add the last sentence version
-            last_sentence_version = sen_versions[-1]
-            if last_sentence_version.text not in [sv.text for sv in filtered_sentence_history[id]]:
-                filtered_sentence_history[id].append(last_sentence_version)
-        for id, sen_versions in filtered_sentence_history.items():
-            for svid, sv in enumerate(sen_versions):
-                if sv.label == 'new' and sv.revision_relevance is False and len(sen_versions) > svid + 1 and \
-                        sen_versions[svid + 1].label != 'deleted':
-                    del sen_versions[svid]
-        return filtered_sentence_history
+            self.filtered_sentence_history[id] = []
+            prev_sv = None
+            for senid, sv in enumerate(sen_versions):
+                if prev_sv is None and len(sen_versions) > 1:
+                    prev_sv = sv
+                elif prev_sv is None and sv is not None and len(sen_versions) == 1:
+                    self.filtered_sentence_history[id].append(sv)
+                elif sv.text is None and len(sen_versions) == 1:
+                    continue
+                elif sv.text is None and prev_sv is not None and len(sen_versions) > 1:
+                    self.filtered_sentence_history[id].append(sv)
+                elif sv.text is not None and prev_sv is not None and len(sen_versions) > 1:
+                    if sv is None:
+                        self.filtered_sentence_history[id].append(sv)
+                    else:
+                        match_ranges = retrieve_match_range(prev_sv.text, sv.text)
+                        match = 0
+                        for mr in match_ranges:
+                            # a small match range may refer to one letter
+                            # which may occur at many positions hence it's ignored
+                            if mr.size > 1:
+                                match += mr.size
+                        prev_sen_len = len(prev_sv.text)
+                        sen_len = len(sv.text)
+                        mismatch = prev_sen_len - match
+                        if mismatch > 3:
+                            self.filtered_sentence_history[id].append(prev_sv)
+                        elif mismatch == 0 and len(match_ranges) > 2 and sen_len - match > 3:
+                            self.filtered_sentence_history[id].append(prev_sv)
+                        if senid + 1 == len(sen_versions) and sv not in self.filtered_sentence_history[id]:
+                            self.filtered_sentence_history[id].append(sv)
+                    prev_sv = sv
+                if len(sen_versions) == 1 and sv not in self.filtered_sentence_history[id]:
+                    self.filtered_sentence_history[id].append(sv)
 
