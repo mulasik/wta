31cde4fd9fd5b5c9cd725383107813cbb961da2b
Malgorzata Anna Ulasik <ulas@zhaw.ch>
2023-08-03 17:24:03 +0200

    improved regex for white space handling in TUs

--- wta/pipeline/regular_expressions.py
+++ wta/pipeline/regular_expressions.py
@@ -1,5 +1,6 @@
 import re
 
-TRAILING_WS_RE = re.compile(r"(\s+|\n+|\t+)\Z")
-ONLY_WS_RE = re.compile(r"\A(\s+|\n+|\t+)\Z")
-INITIAL_WS_RE = re.compile(r"\A(\s+|\n+|\t+)")
+INITIAL_WS_RE = re.compile(r"\A((\s+\n*\t*)+|(\n+\t*)+|(\t+\n*)+)")
+TRAILING_WS_RE = re.compile(r"((\s+\n*\t*)+|(\n+\t*)+|(\t+\n*)+)\Z")
+ONLY_WS_RE = re.compile(r"\A((\s+\n*\t*)+|(\n+\t*)+|(\t+\n*)+)\Z")
+
