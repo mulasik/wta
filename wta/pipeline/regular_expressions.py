6f2149f5d1c7b1957f9f6bab9432b8063be69502
Malgorzata Anna Ulasik <ulas@zhaw.ch>
2023-08-22 11:33:48 +0200

    extended whitespace re

--- wta/pipeline/regular_expressions.py
+++ wta/pipeline/regular_expressions.py
@@ -1,6 +1,9 @@
 import re
 
+# TODO: consider converting text to unicode
+
+
 INITIAL_WS_RE = re.compile(r"\A((\s+\n*\t*)+|(\n+\t*)+|(\t+\n*)+)")
 TRAILING_WS_RE = re.compile(r"((\s+\n*\t*)+|(\n+\t*)+|(\t+\n*)+)\Z")
 ONLY_WS_RE = re.compile(r"\A((\s+\n*\t*)+|(\n+\t*)+|(\t+\n*)+)\Z")
-MIDDLE_WS_RE = re.compile(r"(\n+\t*)+|(\t+\n*)")
+MIDDLE_WS_RE = re.compile(r"(\n+\t*)+|(\r+\n*\t*)+|(\t+\n*)+|(\t+\r*)+")
